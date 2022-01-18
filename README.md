4192efa923bb8ede11fea13c19fc5ae3e7bc0f09
PAUL34913 <paul34913@gmail.com>
2022-01-18 17:07:22 +0000

    git init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Script Repo - Advanced Javascript (ES6)
+
+Collection of scripts from Advanced JavaScript module.
---
+++ arrow-functions-challenge.js
@@ -0,0 +1,36 @@
+
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'maths','english', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', english: 'Joan', art: 'Simon'},
+        results: {science: 93, maths: 95, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 79, art: 95},
+    }
+];
+
+const averagePoints = (arr, subject) => {
+    let count = 0;
+    let total = 0;
+    for (let i in arr) {
+        if (subject in arr[i].results) {
+            total += (arr[i].results[subject]);
+            count++;
+        }
+    }
+    return total / count;
+}
+
+let result = averagePoints(students, 'cad');
+console.log(result);
